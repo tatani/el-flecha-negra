c60cc2c28c5039a1c501d2b1dd2bf6e701521c30
Chris Ruppel <chris.ruppel@gmail.com>
2015-02-04 18:11:21 +0100

    :boom: scaffolding for site

---
+++ Gemfile
@@ -0,0 +1,10 @@
+# Pull gems from RubyGems
+source 'https://rubygems.org'
+
+# Our Sass stack
+gem 'sass', '~>3.4.1'
+gem 'compass', '~>1.1.0.alpha.0'
+gem 'breakpoint', '~>2.5.0'
+
+# We're not using GitHub pages so let's give Jekyll 2.0.0 a whirl
+gem 'jekyll', '~>2.5.3'
---
+++ gulpfile.js
@@ -0,0 +1,48 @@
+var gulp = require('gulp');
+var gutil = require('gulp-util');
+var color = gutil.colors;
+
+// Include Our Plugins
+var compass = require('gulp-compass');
+var paths = require('compass-options').paths();
+var taskListing = require('gulp-task-listing');
+
+console.log(paths);
+
+//////////////////////////////
+// Compass
+//////////////////////////////
+gulp.task('compass', function () {
+  return gulp.src(paths.sass + '/**/*.scss')
+    .pipe(compass({
+      config_file: 'config.rb',
+      bundle_exec: true,
+      sourcemap: false,
+      time: true,
+      css: paths.css,
+      sass: paths.sass,
+      image: paths.img,
+      fonts: paths.fonts
+    }))
+    .on('error', function(error) {
+      // Compass prints the error, so only have to log that we handled it.
+      gutil.log(
+        color.red('compass'),
+        'Error caught. Continuing...'
+      );
+      this.emit('end');
+    })
+    .pipe(gulp.dest(paths.css));
+});
+
+
+// Watch Files For Changes
+gulp.task('watch', function() {
+  gulp.watch(paths.sass + '/**/*.scss', ['compass']);
+});
+
+// Add a task to render the output
+gulp.task('help', taskListing);
+
+// Default task
+gulp.task('default', ['help']);
